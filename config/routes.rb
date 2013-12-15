7771f95d83cabad9f0e1e501c699c00acbc9e8a7
Takahiko Mori <takahiko@centos64.localdomain>
2013-12-15 22:06:58 +0900

    change route to books

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Dviz::Application.routes.draw do
   resources :books
 
-  root 'welcome#index'
+  #root 'welcome#index'
+  root 'books#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
